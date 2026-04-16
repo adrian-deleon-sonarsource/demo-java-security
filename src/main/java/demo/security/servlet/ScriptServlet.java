81b3631c527e594d43d1133a77fe56759026cdc6
Adrian De Leon <adrian.deleon@sonarsource.com>
2026-04-16 09:29:36 -0500

    comments for testing push request

--- src/main/java/demo/security/servlet/ScriptServlet.java
+++ src/main/java/demo/security/servlet/ScriptServlet.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+// adding comment to test git pull request
 
 @WebServlet("/scripts")
 public class ScriptServlet extends HttpServlet {
